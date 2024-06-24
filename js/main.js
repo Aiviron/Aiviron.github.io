6269bb85c95fae73f3b147e429080077f12fdbb2
Aiviron <michael.aiviron@gmail.com>
2024-06-24 04:55:31 +0300

    sec commit

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('data/rucksacks.json')
-    .then(response => response.json())
-    .then(data => {
-      const productsContainer = document.getElementById('products-container');
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("data/rucksacks.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const productsContainer = document.getElementById("products-container");
 
-      data.forEach(rucksack => {
+      data.forEach((rucksack) => {
         const productCard = createProductCard(rucksack);
-        productCard.querySelector('button').onclick = openProductPage;
+        productCard.querySelector("button").onclick = openProductPage;
         productsContainer.appendChild(productCard);
       });
     })
-    .catch(error => {
-      console.error('Ошибка при загрузке данных о рюкзаках:', error);
+    .catch((error) => {
+      console.error("Ошибка при загрузке данных о рюкзаках:", error);
     });
 });
 
 function createProductCard(rucksack) {
-  const productDiv = document.createElement('div');
-  productDiv.classList.add('product');
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("product");
 
-  const image = document.createElement('img');
+  const image = document.createElement("img");
   image.src = rucksack.image;
   image.alt = rucksack.name;
 
-
-  const title = document.createElement('h3');
+  const title = document.createElement("h3");
   title.textContent = rucksack.name;
 
-  const price = document.createElement('p');
+  const price = document.createElement("p");
   price.textContent = `Цена: ${rucksack.price} руб.`;
 
-  const button = document.createElement('button');
-  button.textContent = 'Купить';
+  const button = document.createElement("button");
+  button.textContent = "Купить";
   button.dataset.productId = rucksack.id;
 
   productDiv.appendChild(image);
@@ -44,5 +43,5 @@ function createProductCard(rucksack) {
 
 function openProductPage(event) {
   const productId = event.target.dataset.productId;
-      window.location.href = `pages/product.html?id=${productId}`;
+  window.location.href = `pages/product.html?id=${productId}`;
 }
--- js/product.js
+++ js/product.js
@@ -1,41 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
-  const productId = urlParams.get('id');
+  const productId = urlParams.get("id");
 
   if (productId) {
-    fetch(`data/rucksacks.json`)
-      .then(response => response.json())
-      .then(data => {
-        const product = data.find(rucksack => rucksack.id === productId);
+    fetch("data/rucksacks.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const product = data.find((rucksack) => rucksack.id === productId);
 
         if (product) {
           displayProduct(product);
         } else {
-          console.error('Товар не найден');
+          console.error("Товар не найден");
         }
       })
-      .catch(error => {
-        console.error('Ошибка при загрузке данных о рюкзаках:', error);
+      .catch((error) => {
+        console.error("Ошибка при загрузке данных о рюкзаках:", error);
       });
   } else {
-    console.error('Не указан ID товара');
+    console.error("Не указан ID товара");
   }
 });
 
 function displayProduct(rucksack) {
-  const productContainer = document.getElementById('product-container');
+  const productContainer = document.getElementById("product-container");
 
-  const image = document.createElement('img');
+  const image = document.createElement("img");
   image.src = `images/${rucksack.image}`;
   image.alt = rucksack.name;
 
-  const title = document.createElement('h2');
+  const title = document.createElement("h2");
   title.textContent = rucksack.name;
 
-  const description = document.createElement('p');
+  const description = document.createElement("p");
   description.textContent = rucksack.description;
 
-  const price = document.createElement('p');
+  const price = document.createElement("p");
   price.textContent = `Цена: ${rucksack.price} руб.`;
 
   productContainer.appendChild(image);
@@ -43,4 +43,3 @@ function displayProduct(rucksack) {
   productContainer.appendChild(description);
   productContainer.appendChild(price);
 }
-
